26aaabff73e30792d54ab3e4873dbec7ec563327
Shubham.16 <shubhamlakade1994@gmail.com>
2021-06-12 20:19:02 +0530

    New file add

---
+++ Area.java
@@ -0,0 +1,20 @@
+package All;
+
+public class Area {
+
+	public double Diameter(double radius)
+	{
+		return 2*radius;
+	}
+	
+	public double Cir(double radius)
+	{
+		return 2*3.14*radius;
+	}
+	
+	public double AreaofCircle(double radius)
+	{
+		return 3.14*radius*radius;
+	}
+	
+}
